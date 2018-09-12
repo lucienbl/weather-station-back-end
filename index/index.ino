b78debd4cd49a28beec25d2bf3b7ca070cdc12e4
lucienbl <lucien.blunk.lallet@gmail.com>
2018-09-12 16:56:25 +0200

    update: added LCD support

--- index/index.ino
+++ index/index.ino
@@ -1,8 +1,11 @@
+#include <LiquidCrystal_I2C.h>
 #include <SimpleDHT.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <Wire.h>  
 
 EthernetServer serveur(80);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0xDF, 0xAB};
 
@@ -13,11 +16,20 @@ int pinDHT11 = 2;
 SimpleDHT11 dht11;
 
 void setup() {
+  lcd.begin(); 
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Initializing...");
+    
   Serial.begin (9600);
 
   Ethernet.begin (mac);
   Serial.print("\nLe serveur est sur l'adresse : ");
   Serial.println(Ethernet.localIP());
+  lcd.clear();
+  lcd.print("IP: ");
+  lcd.setCursor(4, 0);
+  lcd.print(Ethernet.localIP());
   serveur.begin();
 }
 
@@ -32,6 +44,9 @@ void loop() {
 
     if (client.connected()) {
       while (client.available()) {
+        lcd.setCursor(0, 1);
+        lcd.print("Client loading...");
+      
         char c=client.read();
         Serial.write(c);
         delay(1);
@@ -56,6 +71,9 @@ void loop() {
 }
 
 void getTempHum() {
+  lcd.setCursor(0,1);
+  lcd.print("Tmp." + (String)temperature + (char)223 + "C " + "Hum." + (int)humidity + "%");
+  
   byte data[40] = {0};
   if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
     Serial.print("Read DHT11 failed");
